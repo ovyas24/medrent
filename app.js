513dde99ea36880a039ffec5aca8cb9c6a53686c
ovyas24 <ovyas24@gmail.com>
2021-03-20 09:37:24 +0530

    Seting up view engine

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const ejs = require("ejs")
+const path = require("path")
 const app = express()
 const port = process.env.PORT||3000
 
-app.get('/',(req,res)=>{
-   res.send('Hello')
-})
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.use("/",require("./routes/user/index"))
+app.use("/admin", require("./routes/admin/index"))
 
 app.listen(port,(req,res)=> console.log(`App running at port ${port}`))
